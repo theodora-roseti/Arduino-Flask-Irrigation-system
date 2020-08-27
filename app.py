be0b7afe6da0535a1ad0b2826c475bcae9cd40ee
Theodora Roseti <44373742+theodora-roseti@users.noreply.github.com>
2020-08-27 15:10:44 +0300

    FLASK-TXT INTERACTION

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 # import SQLAlchemy as SQLAlchemy
 import random
-
+import datetime
+import sqlite3
+import time
+import serial as serial
+import serial
+from serial import Serial
+from time import sleep
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, redirect, url_for
 from sqlalchemy import Column, Integer, Text
-
+__all__ = [serial, SQLAlchemy, Flask, render_template, request, redirect, url_for]
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'hello'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+report_values = [0, 0, 0, 0, 0]
 
 
 class User(db.Model):
@@ -49,6 +56,7 @@ def humidity():
             found_plant.hum_soil = plant_soil_hum
             found_plant.hum_air = plant_air_hum
             db.session.commit()
+            humidity_write(found_plant.hum_soil, found_plant.hum_air)
         return redirect(url_for("index"))
     else:
         return render_template('Humidity.html', title='Humidity')
@@ -57,13 +65,14 @@ def humidity():
 @app.route('/Temperature', methods=['POST', 'GET'])
 def temperature():
     if request.method == "POST":
-        plant_soil_temp = request.form['formControlRange']
+        #plant_soil_temp = request.form['formControlRange']
         plant_air_temp = request.form['formControlRange2']
         found_plant = User.query.filter_by(id=1).first()
         if found_plant:
-            found_plant.temp_soil = plant_soil_temp
+            #found_plant.temp_soil = plant_soil_temp
             found_plant.temp_air = plant_air_temp
             db.session.commit()
+            temperature_write(found_plant.temp_air)
         return redirect(url_for("index"))
     else:
         return render_template('Temperature.html', title='Temperature')
@@ -71,23 +80,30 @@ def temperature():
 
 @app.route('/Report')
 def report():
-    found_plant = User.query.filter_by(id=1).first()
-    if found_plant:
-        temp_air = found_plant.temp_soil
-        temp_soil = found_plant.temp_air
-        hum_soil = found_plant.hum_soil
-        hum_air = found_plant.hum_air
+    report_read()
 
-    return render_template('GenReport.html', title='Report', t_air=temp_air, t_soil=temp_soil, h_air=hum_air, hum_soil=hum_soil)
+    temp_air = report_values[1]
+    heath_index = report_values[2]
+    hum_air = report_values[0]
+    hum_soil = report_values[3]
+    h_per = report_values[4]
+
+    return render_template('GenReport.html', title='Report', t_air=temp_air, t_soil=heath_index, hum_air=hum_air, hum_soil=hum_soil, hum_percent=h_per)
 
 
 @app.route('/Manual', methods=['POST', 'GET'])
 def manual():
     if request.method == "POST":
         if request.form['status'] == 'buttonOn':
+            f = open("C:\\Users\\theo\\Desktop\\arduino\\onoff.txt", "w") #schimb fisier
+            f.write("1")
+            f.close()
             return redirect(url_for("index"))
         elif request.form['status'] == 'buttonOff':
-            pass
+            f = open("C:\\Users\\theo\\Desktop\\arduino\\onoff.txt", "w") #schimb fisier
+            f.write("0")
+            f.close()
+            return redirect(url_for("index"))
     elif request.method == "GET":
         return render_template('Manual.html', title='Manual On/Off')
 
@@ -104,6 +120,54 @@ def addplant():
         return render_template('AddPlant.html', title='Add a new plant')
 
 
+def read_arduino():
+    arduinoData = serial.Serial('COM9', 9600, timeout=0.1)
+    while arduinoData.inWaiting:
+        val = arduinoData.readline().decode('ascii')
+        if len(val) == 13:
+            return val
+
+
+def humidity_write(value_soil, value_air):
+    total_air = 'Air Humidity: {}'.format(value_air)
+    total_soil = 'Soil Humidity: {}'.format(value_soil)
+
+    with open("C:\\Users\\theo\\Desktop\\arduino\\config.txt", 'r+') as f:
+        contents_air = f.read().replace('Air Humidity: ', total_air)
+        f.seek(0)
+        f.truncate()
+        f.write(contents_air)
+    with open("C:\\Users\\theo\\Desktop\\arduino\\config.txt", 'r+') as f:
+        contents_soil = f.read().replace('Soil Humidity: ', total_soil)
+        f.seek(0)
+        f.truncate()
+        f.write(contents_soil)
+
+
+def temperature_write(value_air):
+    total_air = 'Air Temperature: {}'.format(value_air)
+
+    with open("C:\\Users\\theo\\Desktop\\arduino\\config.txt", 'r+') as f:
+        contents_air = f.read().replace('Air Temperature: ', total_air)
+        f.seek(0)
+        f.truncate()
+        f.write(contents_air)
+
+
+def report_read():
+    i = 0
+
+    with open("C:\\Users\\theo\\Desktop\\arduino\\Measurement_1.txt", "r") as a_file:
+        for line in a_file:
+            if ':' in line:
+                mock, value = line.rstrip().split(':')
+                report_values[i] = value
+                i = i+1
+            if i == 5:
+                break
+
+
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
+
